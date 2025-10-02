b0eef1729903efec89590cf2add2ad361629d701
maramrmilad7 <maro12787@gmail.com>
2025-10-02 15:21:30 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NoteApp());
+}
+
+class NoteApp extends StatelessWidget {
+  const NoteApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
