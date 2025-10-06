eb0b106813b3fdb807a5c1259878e3324973fabc
maramrmilad7 <maro12787@gmail.com>
2025-10-06 12:36:36 +0200

    init hive and open box

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:noteapp/views/notes_view.dart';
 
-void main() {
+void main()async {
+  await Hive.initFlutter();
+  await Hive.openBox('notes'); // = collection infirestore
   runApp(const NoteApp());
 }
 
